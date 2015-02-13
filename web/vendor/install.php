4eaad1270757d6ecd6bd51cef9f0a1bcedd072f9
Mitchell Morrell <morrellm@msoe.edu>
2015-02-13 14:24:22 -0600

    Changed to phpunit so code coverage would work

--- web/vendor/install.php
+++ web/vendor/install.php
@@ -42,7 +42,7 @@ function install($src, $destFilename)
 }
 
 //codeception install
-install("http://codeception.com/codecept.phar", "codecept.phar");
-install("http://xdebug.org/files/php_xdebug-2.2.7-5.4-vc9.dll", "xdebug.dll");
+install("https://phar.phpunit.de/phpunit.phar", "phpunit.phar");
+//install("http://xdebug.org/files/php_xdebug-2.2.7-5.4-vc9.dll", "xdebug.dll");
 
 ?>
